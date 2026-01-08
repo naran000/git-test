cdc8d42d27fa72d8ae8d878e591f814507683618
naran000 <naranonly@gmail.com>
2026-01-08 18:15:00 +0800

    modify file ”English“

--- English_to_uppercase.py
+++ English_to_uppercase.py
@@ -1,3 +1,6 @@
+import re  # 必须添加这一行！
+from pypinyin import pinyin, Style, lazy_pinyin
+
 def uppercase_english(text):
     # 匹配所有 a-z 的字母并替换为大写
     return re.sub(r'[a-z]+', lambda m: m.group(0).upper(), text)
@@ -5,4 +8,3 @@ def uppercase_english(text):
 # 测试
 test_text = "Python is 优秀的 programming language!"
 print(uppercase_english(test_text))
-print("hello world!!!")
